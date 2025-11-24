9795508c3413542f57848423adb98b7fea995fa3
Tenjeeb K. C. <tenjeeb15@gmail.com>
2025-11-24 15:55:15 +0545

    Commit

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 let wind = 0;   // global varible to trach wind angle over time
+
 function setup() {
-    createCanvas(400, 400);  // drawing areas (the canvas)
+    createCanvas(800, 800);  // drawing areas (the canvas)
     angleMode(DEGREES) // Used degrees for rotation later
+    stroke('white');  // tree color
 }
 
 function draw(){
-    background('#0a192f');   // Navy blue
+    background('#0a192f');   
+
+    wind += 0.9  // wind intensity
 
     // Put the drawing start point at the bottom center
     translate(width / 2, height);
 
-    stroke('#64ffda');  // tree color
-    strokeWeight(6);   // Trunk thickness
-
-    drawBranch(120)   // Start drawing the branch of length 120
+    drawBranch(120, wind)   // Start drawing the branch of length 120
 
 }
 
-function drawBranch(len) {
-    strokeWeight(map(len, 10, 120, 1, 6))  // thinner branch when smaller
+function drawBranch(len, offset) {
+    strokeWeight(map(len, 10, 120, 1, 3))  // thinner branch when smaller
 
     line(0, 0, 0, -len);  // draw branch line upwards
     translate(0, -len);  // move to the end of this branch
 
-    if(len > 10){  // Stop recursion if branch is small
+    if(len > 7){  // Stop recursion if branch is small
+
+        // Calculate sway angle based on wind and branch length
+        const sway = sin(offset) * (15 / (120 / len))
+        
         push(); // save current drawing state
-        rotate(25);   // rotate right
-        drawBranch(len * 0.7)  // draw smaller right branch
-        pop()   // restore drawing 
+        rotate(25 + sway);   // rotate right
+        drawBranch(len * 0.7, offset + 2);  // draw smaller right branch
+        pop();   // restore drawing 
         
         push()
-        rotate(-25)  // rotate left
-        drawBranch(len * 0.7)  // draw smaller left branch
+        rotate(-25 - sway)  // rotate left
+        drawBranch(len * 0.7, offset + 2)  // draw smaller left branch
         pop()
     }
     
